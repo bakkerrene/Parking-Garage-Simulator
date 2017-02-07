f7f840565e9b73f3b2a3611fdb313c74274103f5
Rick Zwaneveld <r.zwaneveld@st.hanze.nl>
2017-02-07 09:43:27 +0100

    Javadoc

--- src/parkeersimulator/Rect.java
+++ src/parkeersimulator/Rect.java
@@ -1,5 +1,14 @@
 package parkeersimulator;
 
+/**
+ * This class is used in CarparkView
+ * It calculates the parkingspots rectangles
+ * 
+ * @author Rick Zwaneveld
+ * @version 07/03/2017
+ */
+
+
 public class Rect {
 
 	public int x, y;
