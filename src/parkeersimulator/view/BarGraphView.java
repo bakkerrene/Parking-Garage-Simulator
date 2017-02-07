fec212938efaf44a2512b54c532e475f6f57ebd9
Rene Bakker <renebakker150@gmail.com>
2017-02-07 09:58:47 +0100

    barGraph

--- src/parkeersimulator/view/BarGraphView.java
+++ src/parkeersimulator/view/BarGraphView.java
@@ -38,11 +38,13 @@ public class BarGraphView extends AbstractView {
 		super.paintComponent(g);
 		
 		carCounter = model.getTotalCars();
+		double height = getHeight();
+		double sizePerValue = height / totalPlaces;
 		
 		int x = model.getNumberOfOpenSpots();
 
 		int y = totalPlaces - x;
-		double height = getHeight();
+		
 
 		int freeSpots = (int) ((height / totalPlaces) * x);
 		int topFree = (int) (height - freeSpots);
@@ -53,25 +55,35 @@ public class BarGraphView extends AbstractView {
 		int blueCount = carCounter.get("pass");
 		int greenCount = carCounter.get("handi");
 		int yellowCount = carCounter.get("res");
+		
+		System.out.println(sizePerValue);
+		redCount *= sizePerValue;
+		System.out.println(redCount);
+		blueCount *= sizePerValue;
+		System.out.println(blueCount);
+		greenCount *= sizePerValue;
+		System.out.println(greenCount);
+		yellowCount *= sizePerValue;
+		System.out.println(yellowCount);
 
-		double topNonFreeRed = (height - redCount);
-		int topNonFreeBlue = (int) (height - blueCount);
-		int topNonFreeGreen = (int) (height - greenCount);
-		int topNonFreeYellow = (int) (height - yellowCount);
+		int topNonFreeRed = (int) (height - (redCount));
+		int topNonFreeBlue = (int) (height - (blueCount));
+		int topNonFreeGreen = (int) (height - (greenCount));
+		int topNonFreeYellow = (int) (height - (yellowCount));
 
 		g.setColor(Color.WHITE);
 		g.fillRect(20, topFree, 20, freeSpots);
 
 		g.setColor(ParkingSpot.getColorForType(ParkingSpot.TYPE_AD_HOC));
-		g.fillRect(60, (int) topNonFreeRed, 20, nonFreeSpots);
+		g.fillRect(60, topNonFreeRed, 20, redCount);
 
 		g.setColor(ParkingSpot.getColorForType(ParkingSpot.TYPE_PASS));
-		g.fillRect(100, topNonFreeBlue, 20, nonFreeSpots);
+		g.fillRect(100, topNonFreeBlue, 20, blueCount);
 
 		g.setColor(ParkingSpot.getColorForType(ParkingSpot.TYPE_HANDI));
-		g.fillRect(140, topNonFreeGreen, 20, nonFreeSpots);
+		g.fillRect(140, topNonFreeGreen, 20, greenCount);
 
 		g.setColor(ParkingSpot.getColorForType(ParkingSpot.TYPE_RES));
-		g.fillRect(180, topNonFreeYellow, 20, nonFreeSpots);
+		g.fillRect(180, topNonFreeYellow, 20, yellowCount);
 	}	
 }
